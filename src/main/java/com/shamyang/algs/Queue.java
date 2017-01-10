359a951d9eabeb11da5f78f6649e01648a64ec5e
yangzhibin <292292yang@163.com>
2017-01-10 23:03:16 +0800

    增加入队列 出队列方法

--- src/main/java/com/shamyang/algs/Queue.java
+++ src/main/java/com/shamyang/algs/Queue.java
@@ -1,5 +1,7 @@
 package com.shamyang.algs;
 
+import java.util.NoSuchElementException;
+
 /**
  * <p>
  *     队列
@@ -8,9 +10,10 @@ package com.shamyang.algs;
  */
 public class Queue<T> {
 
+
     private class Node<T>{
         private T item;
-        private int size;
+        private Node<T> next;
     }
 
     private Node<T> first;
@@ -23,5 +26,47 @@ public class Queue<T> {
         first=null;
         last=null;
         size=0;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return first==null;
+    }
+
+    public void enqueue(T item){
+        Node<T> oldLast=last;
+        last=new Node<>();
+        last.item=item;
+        last.next=null;
+        if(isEmpty()) first=last;
+        else oldLast.next=last;
+        size++;
+    }
+
+    public T dequeue(){
+        if(isEmpty()) throw new NoSuchElementException("Queue is empty");
+        T item=first.item;
+        first=first.next;
+        size--;
+        return item;
+    }
+
+    public T peek(){
+        if (isEmpty()) throw new NoSuchElementException("Queue is empty");
+        return first.item;
+    }
+
+    public static void main(String[] args) {
+        Queue<Integer> queue=new Queue<>();
+        for (int i = 0; i < 10; i++) {
+            queue.enqueue(i);
+        }
+
+        while (!queue.isEmpty()){
+            System.out.println(queue.dequeue());
+        }
     }
 }
